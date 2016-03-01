7de4430636b0513183567822c00e08ffe2bcc43e
Tadrix <madlord@gmail.com>
2016-03-01 15:57:43 +0200

    new changes

--- js/views/default.js
+++ js/views/default.js
@@ -6,14 +6,17 @@ define(['jquery', 'underscore', 'backbone', 'mustache', '/js/models/page.js', 't
     initialize: function() {
       this.data = {
         main: {
+          head: 'site',
           title: 'main title',
           content: 'Lorem ipsum 1'
         },
         about: {
+          head: 'site / about',
           title: 'about title',
           content: 'Lorem ipsum 2'
         },
         contact: {
+          head: 'site / contact',
           title: 'contact title',
           content: 'Lorem ipsum 3'
         }
